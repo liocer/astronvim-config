f068927c5af8196753e7adf2242c2683fa3e7716
Rob Brown <rob.brown@eu.clara.net>
2023-05-22 11:51:53 +0100

    Made it a bit more fancy

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,33 +1,31 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
   -- lets have some themes...
-  "Mofiqul/dracula.nvim",
-  "folke/tokyonight.nvim",
-  "mhartington/oceanic-next",
-  "marko-cerovac/material.nvim",
-  "EdenEast/nightfox.nvim",
-  "sainnhe/sonokai",
-  "ellisonleao/gruvbox.nvim",
-  "Shatur/neovim-ayu",
+  { "Mofiqul/dracula.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "Shatur/neovim-ayu" },
+  { "mhartington/oceanic-next" },
+  {
+    "marko-cerovac/material.nvim",
+    init = function()
+      vim.g.material_style = "deep ocean" -- darker is nice too
+    end
+  },
+  { "EdenEast/nightfox.nvim" },
+  { "sainnhe/sonokai" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- other stuff
+  -- { "andweeb/presence.nvim" }, -- Discord presence plugin
   {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
     end
-
   }
-
 }
