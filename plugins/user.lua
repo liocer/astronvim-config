f649a44f43ddcff40c1b11f23d77cf9d2a939916
Rob Brown <rob.brown@eu.clara.net>
2023-07-09 16:10:15 +0100

    Updated things for NVIM QT

--- plugins/user.lua
+++ plugins/user.lua
@@ -2,19 +2,36 @@ return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- lets have some themes...
-  { "Mofiqul/dracula.nvim" },
-  { "folke/tokyonight.nvim" },
-  { "Shatur/neovim-ayu" },
-  { "mhartington/oceanic-next" },
+  { 
+    "Mofiqul/dracula.nvim",
+    lazy = false,
+  },
+  { 
+    "folke/tokyonight.nvim",
+    lazy = false,
+  },
+  { 
+    "Shatur/neovim-ayu",
+    lazy = false,
+  },
   {
     "marko-cerovac/material.nvim",
+    lazy = false,
     init = function()
       vim.g.material_style = "deep ocean" -- darker is nice too
     end
   },
-  { "EdenEast/nightfox.nvim" },
-  { "sainnhe/sonokai" },
-  { "ellisonleao/gruvbox.nvim" },
+  { 
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+  },
+  { 
+    "ellisonleao/gruvbox.nvim"
+  },
+  {
+    "oxfist/night-owl.nvim",
+    lazy = false,
+  },
 
   -- other stuff
   -- { "andweeb/presence.nvim" }, -- Discord presence plugin
